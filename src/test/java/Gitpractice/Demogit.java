ed9db3da28c291e4863e515dfc22fdd0c58c1ae8
vardhini1991 <vardhininaga108@gmail.com>
2023-04-12 16:50:29 +0530

    vardhini git commit n push

---
+++ src/test/java/Gitpractice/Demogit.java
@@ -0,0 +1,13 @@
+package Gitpractice;
+
+import org.testng.annotations.Test;
+
+public class Demogit 
+{
+	@Test
+	public void demogit()
+	{
+		System.out.println("this is vardhini github");
+	}
+
+}
