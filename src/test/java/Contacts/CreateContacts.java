dd5ba6682fd1a78086cfc5101aae54445e06cdd8
PC <shaiksana9696@gmail.com>
2023-04-14 11:17:00 +0530

    commited branche by sana

--- src/test/java/Contacts/CreateContacts.java
+++ src/test/java/Contacts/CreateContacts.java
@@ -1,5 +1,12 @@
 package Contacts;
 
+import org.testng.annotations.Test;
+
 public class CreateContacts {
+	@Test
+	public void createContacts()
+	{
+		System.out.println("contact created");
+	}
 
 }
