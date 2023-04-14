abb74ddc2b9eb597b29a42aa4ca2b751f6295c36
vardhini <vardhininaga108@gmail.com>
2023-04-14 11:01:50 +0530

    branch practice committed

---
+++ src/test/java/Gitpractice/Branchingpractice.java
@@ -0,0 +1,13 @@
+package Gitpractice;
+
+import org.testng.annotations.Test;
+
+public class Branchingpractice 
+{
+	@Test
+	public void branch()
+	{
+		System.out.println("branching practice");
+	}
+
+}
